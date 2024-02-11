a3b4deae41888100fdcc6880d0c9c0dbe9751af7
Tahsan Ahmed <159710814+tahsan365@users.noreply.github.com>
2024-02-11 16:42:33 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,12 @@ export default function Home() {
 			<div className='row d-flex justify-content-center'>
 				<div className='col-lg-8 create-quiz'>
 					<div className='quiz-header'>
-						<h1>{'title'}</h1>
-						<p>{'description'}</p>
+						<h1>Share your feedback</h1>
+						<p>Feel free to share your opinion</p>
 					</div>
 
 					<div className='add-questions'>
-						<button className='btn btn-success me-2'>Add Question</button>
-						<button className='btn btn-warning me-2'>Settings</button>
-						<button className='btn btn-dark text-light'>Publish Quiz</button>
+						<button className='btn btn-success me-2'>submit feedback</button>
 					</div>
 				</div>
 			</div>
